27b01a42cf178f4a7d95477d95c1952d79b6ef64
Jose Corredor <jose96corrza@gmail.com>
2021-10-25 23:23:01 -0500

    Cambios en vivo para el despliegue en AWS Amplify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Prueba de despliegue de app en React usando AWS Amplify
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Prueba de despliegue de app en React usando AWS Amplify
+          Aqui va un hermoso Link
         </a>
       </header>
     </div>
